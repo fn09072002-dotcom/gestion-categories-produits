06ad22433d585323fdf3c55ed293c3d848ad3866
fatou <fn09072002@gmail.com>
2026-06-30 19:50:16 +0000

    feat: initialisation deux categories avec et sans produits

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$categories = [
+
+   0 => [
+        "code" => "CAT001",
+        'nom' => "Alimentaire",
+        'produits' => [
+            ['nom'=>'lait', 'reference'=>'ref1', 'prix'=>2000, 'quantite'=>10],
+            ['nom'=>'cafe', 'reference'=>'ref2', 'prix'=>1500, 'quantite'=>8]
+        ]
+    ],
+
+      1=> [
+        "code" => "CAT002",
+        'nom' => "hygienique",
+        'produits' => []
+    ]
+];
